dc1083414fe3a143d7df3cbb8a2a7058cff789fa
Guglielmo <guglielmocamporese@gmail.com>
2018-10-26 17:54:42 +0200

    update

--- utilities.py
+++ utilities.py
@@ -11,7 +11,7 @@ def download_dataset():
     '''
     # File Configuration
     os.system('mkdir ~/.kaggle')
-    os.system('cp kaggle.json ~/.kaggle/')
+    os.system('cp human_protein_atlas_image_classification/kaggle.json ~/.kaggle/')
 
     os.system('chmod 600 ~/.kaggle/kaggle.json')
 
